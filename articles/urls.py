56ae289b533a8d586cca9fe4af33edd52d8ecde5
mehdi derikvand <yonotomakarde@gmail.com>
2024-11-14 00:24:58 +0330

    added slug urls and views

--- articles/urls.py
+++ articles/urls.py
@@ -2,10 +2,11 @@
 from django.urls import path
 from . import views
 
+app_name = "articles"
 
 urlpatterns = [
     
-    path('' , views.articles_list),
-    path('<slug>',views.article_detail),
+    path('' , views.articles_list, name="list"),
+    path('<slug>',views.article_detail, name= "detail"),
 
 ]
